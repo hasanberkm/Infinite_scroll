6a783791e68f00551d34a940049610b9268f9522
Hasan Berk Mahmutoğlu <hasan-lincoln-gs@hotmail.com>
2019-10-18 07:03:48 +0300

    Update infinite.js

--- infinite.js
+++ infinite.js
@@ -1,18 +1,13 @@
 page=0; //Page indicator
   
-$.post("url_is_here", { process:"getPosts" page: page }, function(data){
- if( data.indexOf( "No have any more." ) == -1 )
- {
-   $(".ClassName_or_#IdName").append(data);
-   page += 10
-
-
- }
- else
- {
-   $(".process-comm").html("<p>"+data+"<p>");
- }
+$.post("select.php", { process:"getPosts" page: page }, function(data){
+  if( data.indexOf( "No have any more." ) == -1 )
+  {
+    $(".postArea").append(data);
+    page += 10
+  }
+  else
+  {
+    $(".loadingArea").html("<img src='loading.svg'><p>"+data+"<p>");
+  }
 })
-
-
-}) 
