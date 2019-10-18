97f0907d9aa5fc82e9a7bc1c3f58306cfcd2a632
Hasan Berk Mahmutoğlu <hasan-lincoln-gs@hotmail.com>
2019-10-18 07:21:17 +0300

    Update infinite.js

--- infinite.js
+++ infinite.js
@@ -1,13 +1,26 @@
 page=0; //Page indicator
-  
-$.post("select.php", { process:"getPosts" page: page }, function(data){
-  if( data.indexOf( "No have any more." ) == -1 )
-  {
-    $(".postArea").append(data);
-    page += 10
-  }
-  else
-  {
-    $(".loadingArea").html("<img src='loading.svg'><p>"+data+"<p>");
-  }
+
+//Get first Post
+selectPosts();
+
+//Get posts every time you come under the div
+$(document).ready(function(){
+  $(window).scroll(function(){		
+    if((window.innerHeight + window.scrollY) >= document.body.scrollHeight)
+		 selectPosts();
+  });	
 })
+
+function selectPosts()
+{
+  $.post("select.php", { process: "getPosts", page: page }, function(data){
+    if( data.indexOf( "No Have Post Any More." ) == -1 )
+    {
+      $(".postArea").append(data);
+      page += 10
+    }
+
+    else
+      $(".loadingArea").html("<img src='loading.svg'><p>"+data+"<p>");
+  })
+}
