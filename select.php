8e849ae365c404e542457281fabf6a196bb3ac57
Hasan Berk Mahmutoğlu <hasan-lincoln-gs@hotmail.com>
2019-10-18 06:54:48 +0300

    Update select.php

--- select.php
+++ select.php
@@ -4,6 +4,24 @@ if(isset($_POST))
 {
   if(isset($_POST["process"]) && $_POST["process"] == "getPosts")
   {
+    $page = $_POST["page"];
+    $posts = $pdo->prepare("SELECT * FROM posts ORDER BY postID DESC LIMIT $page, 10");
+    $posts->execute();
+    
+    if($posts->rowCount() > 0)
+    {
+      foreach($posts as $p)
+      {
+        echo "
+        <p>".$p["postTitle"]."<p>
+        <p>".$p["postText"]."<p>
+";
+      } 
+    }
+    else
+    {
+      echo "No have any more.";
+    }
   }
 }
 ?>
